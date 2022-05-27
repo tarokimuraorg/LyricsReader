6f6b4f1e7d66d6d35ce57c8ecb862356aee2c1b6
Taro Kimura <tarokimuraorg@outlook.com>
2022-05-27 13:52:06 +0900

    エラー制御を追加

--- GetLyrics.py
+++ GetLyrics.py
@@ -1,39 +1,57 @@
 import requests
 from bs4 import BeautifulSoup
 import urllib
+import re
+from ErrorMessageCreator import ErrorMessageCreator
 
 class GetLyrics:
 
     def __init__(self, artist : str, title : str):
 
-        self.__artist = artist.strip()
-        self.__artist = self.__artist.replace(' ', '+')
-        self.__artist = self.__artist.replace('　', '+')
-        self.__artist = urllib.parse.quote(self.__artist)
+        out_artist = artist.strip()
+        out_artist = re.sub('　+',' ',out_artist)
+        out_artist = re.sub(' +',' ',out_artist)
+        out_artist = urllib.parse.quote(out_artist)
 
-        self.__title = title.strip()
-        self.__title = self.__title.replace(' ', '+')
-        self.__title = self.__title.replace('　', '+')
-        self.__title = urllib.parse.quote(self.__title)
+        out_title = title.strip()
+        out_title = re.sub('　+',' ',out_title)
+        out_title = re.sub(' +',' ',out_title)
+        out_title = urllib.parse.quote(out_title)
 
-    def text(self) -> str:
-        
         url = 'https://www.google.com/search?q='
         url = url + urllib.parse.quote('歌詞')
         url = url + '+'
-        url = url + self.__artist
+        url = url + out_artist
         url = url + '+'
-        url = url + self.__title
+        url = url + out_title
 
-        response = requests.get(url)
-        response.encoding = response.apparent_encoding
+        self.url = url
+        self._emcreator = ErrorMessageCreator()
 
-        soup = BeautifulSoup(response.text, 'html.parser')
-        data = soup.find_all('div',class_='hwc')
+    def text(self) -> str:
+        
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()
+            response.encoding = response.apparent_encoding
 
-        result = ''
+            soup = BeautifulSoup(response.text, 'html.parser')
+            data = soup.find_all('div',class_='hwc')
 
-        for d in data:
-            result += d.text
+            result = ''
 
-        return result.strip()
+            for d in data:
+                result += d.text
+
+            result = result.strip()
+
+            if result:
+                return result.strip()
+            
+            raise ValueError(self._emcreator.message('GetLyrics','text','取得エラー','取得したデータが空です。'))
+
+        except requests.exceptions.RequestException as e:
+            print('{}'.format(self._emcreator.message('GetLyrics','text','通信エラー','{}}'.format(e))))
+        
+        except ValueError as e:
+            print('{}'.format(e))

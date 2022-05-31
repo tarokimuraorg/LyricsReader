a7adfa6c7f3c797f00023687713137f019b48b72
Taro Kimura <tarokimuraorg@outlook.com>
2022-05-31 14:26:23 +0900

    エラー制御を追加
    曲のタイトル または アーティスト名に空文字列が入力された場合、エラーメッセージを返す。

--- GetLyrics.py
+++ GetLyrics.py
@@ -3,55 +3,72 @@ from bs4 import BeautifulSoup
 import urllib
 import re
 from ErrorMessageCreator import ErrorMessageCreator
+from StringConvertor import StringConvertor
 
 class GetLyrics:
 
     def __init__(self, artist : str, title : str):
 
         out_artist = artist.strip()
-        out_artist = re.sub('　+',' ',out_artist)
-        out_artist = re.sub(' +',' ',out_artist)
-        out_artist = urllib.parse.quote(out_artist)
-
         out_title = title.strip()
-        out_title = re.sub('　+',' ',out_title)
-        out_title = re.sub(' +',' ',out_title)
-        out_title = urllib.parse.quote(out_title)
 
-        url = 'https://www.google.com/search?q='
-        url = url + urllib.parse.quote('歌詞')
-        url = url + '+'
-        url = url + out_artist
-        url = url + '+'
-        url = url + out_title
+        strconvertor = StringConvertor()
 
-        self.url = url
-        self._emcreator = ErrorMessageCreator()
+        self.__emcreator = ErrorMessageCreator()
+        self.url = ''
+
+        if out_artist and out_title:
+            
+            out_artist = strconvertor.toHankaku(out_artist)
+            out_artist = out_artist.replace('.','&#46;')
+            out_artist = re.sub('　+',' ',out_artist)
+            out_artist = re.sub(' +',' ',out_artist)
+            out_artist = urllib.parse.quote(out_artist)
+
+            out_title = strconvertor.toHankaku(out_title)
+            out_title = out_title.replace('.','&#46;')
+            out_title = re.sub('　+',' ',out_title)
+            out_title = re.sub(' +',' ',out_title)
+            out_title = urllib.parse.quote(out_title)
+
+            url = 'https://www.google.com/search?q='
+            url = url + urllib.parse.quote('歌詞')
+            url = url + '+'
+            url = url + out_artist
+            url = url + '+'
+            url = url + out_title
+
+            self.url = url
 
     def text(self) -> str:
-        
+
         try:
-            response = requests.get(self.url)
-            response.raise_for_status()
-            response.encoding = response.apparent_encoding
+            if self.url:
 
-            soup = BeautifulSoup(response.text, 'html.parser')
-            data = soup.find_all('div',class_='hwc')
+                response = requests.get(self.url)
+                response.raise_for_status()
+                response.encoding = response.apparent_encoding
 
-            result = ''
+                soup = BeautifulSoup(response.text, 'html.parser')
+                data = soup.find_all('div',class_='hwc')
 
-            for d in data:
-                result += d.text
+                result = ''
 
-            result = result.strip()
+                for d in data:
+                    result += d.text
 
-            if result:
-                return result.strip()
+                result = result.strip()
+
+                if result:
+                    return result.strip()
             
-            raise ValueError(self._emcreator.message('GetLyrics','text','取得エラー','取得したデータが空です。'))
+                raise ValueError(self.__emcreator.message('GetLyrics','text','取得エラー','取得したデータが空です。'))
 
-        except requests.exceptions.RequestException as e:
-            print('{}'.format(self._emcreator.message('GetLyrics','text','通信エラー','{}}'.format(e))))
+            else:
+                raise ValueError(self.__emcreator.message('GetLyrics','init','引数エラー','アーティスト名 または 曲のタイトルが入力されていません。'))
+
+        except requests.exceptions.RequestException:
+            print('{}'.format(self.__emcreator.message('GetLyrics','text','通信エラー','データの取得に失敗しました。')))
         
         except ValueError as e:
             print('{}'.format(e))

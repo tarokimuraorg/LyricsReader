3cd7cd2ab2f6508c686bd7de5d048ff28d0db208
Taro Kimura <tarokimuraorg@outlook.com>
2022-05-31 14:39:52 +0900

    テスト中
    多様な文字に耐えうるか、実験中。

--- test.py
+++ test.py
@@ -1,6 +1,25 @@
 from GetLyrics import GetLyrics
+from WriteLyrics import WriteLyrics
 
-result = GetLyrics('Do As Infinity','夜鷹の夢').text()
+lyrics = ''
+artist = '竹原ピストル'
 
-if result:
-    print(result)
+#title = 'たった二種類の金魚鉢'
+#title = 'ＯＮ　ＴＨＥ　ＲＯＡＤ'
+#title = 'あっかんべ、だぜ故郷'
+title = 'あ。っという間はあるさ'
+
+objlyrics = GetLyrics(artist,title)
+
+print(objlyrics.url)
+print()
+
+lyrics = objlyrics.text()
+
+if lyrics:
+
+    print(lyrics)
+    print()
+
+    if WriteLyrics(lyrics, '{}.txt'.format(title)).onTextFile():
+        print('歌詞をテキストファイルに書き込みました。')

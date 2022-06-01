61e0951279590a05dd1bde927e961b649cd00bb4
Taro Kimura <tarokimuraorg@outlook.com>
2022-06-01 14:36:57 +0900

    テスト中
    様々なタイトルに対応するか、テスト中

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from turtle import title
 from GetLyrics import GetLyrics
 from WriteLyrics import WriteLyrics
 
@@ -7,7 +8,12 @@ artist = '竹原ピストル'
 #title = 'たった二種類の金魚鉢'
 #title = 'ＯＮ　ＴＨＥ　ＲＯＡＤ'
 #title = 'あっかんべ、だぜ故郷'
-title = 'あ。っという間はあるさ'
+#title = 'あ。っという間はあるさ'
+#title = 'いくぜ！いくか！いこうよ！'
+#title = 'Amazing Grace' --- 取得エラー
+#title = 'It\'s my life'
+#title = '俺のアディダス～人としての志～ (Album ver.)'
+title = 'オーバー・ザ・オーバー'
 
 objlyrics = GetLyrics(artist,title)
 
@@ -21,5 +27,11 @@ if lyrics:
     print(lyrics)
     print()
 
-    if WriteLyrics(lyrics, '{}.txt'.format(title)).onTextFile():
+    """
+    if WriteLyrics('', '').onTextFile():
+        print('歌詞をテキストファイルに書き込みました。')
+
+    """
+    
+    if WriteLyrics(lyrics, 'lyrics/{}.txt'.format(title)).onTextFile():
         print('歌詞をテキストファイルに書き込みました。')

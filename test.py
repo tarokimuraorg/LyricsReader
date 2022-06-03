a4526e2a686424b97d6c96648158c38959dfdda6
Taro Kimura <tarokimuraorg@outlook.com>
2022-06-03 13:55:40 +0900

    テストパターンを追加

--- test.py
+++ test.py
@@ -13,7 +13,30 @@ artist = '竹原ピストル'
 #title = 'Amazing Grace' --- 取得エラー
 #title = 'It\'s my life'
 #title = '俺のアディダス～人としての志～ (Album ver.)'
-title = 'オーバー・ザ・オーバー'
+#title = 'オーバー・ザ・オーバー'
+#title = '俺、間違ってねえよな?'
+#title = 'おーい！おーい！！'
+#title = 'カウント10'
+#title = 'きーぷ、うぉーきんぐ！！'
+#title = 'Gimme the mic !!'
+#title = '午前2時 私は今 自画像に描かれた自画像' --- 取得エラー
+#title = '最期の一手 ～聖の青春～'
+#title = '悄気る街、舌打ちのように歌がある。'
+#title = 'STAY FREE!!'
+#title = '全て身に覚えのある痛みだろう？'
+#title = '高千穂峠のこいのぼり～ワルフザケガスギルMIX ～'
+#title = 'ちぇっく！'
+#title = '父から娘へ～さや侍の手紙～'
+#title = 'テイク イット イージー'
+#title = 'トム・ジョード'
+#title = 'ドライブトライブ ～初代機材車、二郎号に捧ぐ～'
+#title = 'どーん！とやってこい、ダイスケ！'
+#title = '狼煙(朗読) ～Live at京都大作戦 2017～'
+#title = 'Here we go!!'
+#title = 'Float Like a Buttrefly, Sting Like a Bee!!'
+#title = 'へっちゃらさ、ベイビー'
+#title = '藍色のハンカチ '
+title = ''
 
 objlyrics = GetLyrics(artist,title)
 
@@ -24,9 +47,11 @@ lyrics = objlyrics.text()
 
 if lyrics:
 
+    """
     print(lyrics)
     print()
-
+    """
+    
     """
     if WriteLyrics('', '').onTextFile():
         print('歌詞をテキストファイルに書き込みました。')
